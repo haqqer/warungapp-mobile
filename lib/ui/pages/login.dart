f029e40f953f49dd501afb1524846fc377f8c7cc
haqqer <ilhamserbaguna@gmail.com>
2020-06-18 13:53:31 +0700

    update login page

--- lib/ui/pages/login.dart
+++ lib/ui/pages/login.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:warungapp_mobile/ui/pages/home.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,6 +11,39 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _isLoading = false;
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  signIn(String email, String pass) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    Map data = {
+      'email': email,
+      'password': pass
+    };
+
+    var jsonResponse = null;
+    String url = sharedPreferences.getString('url')+'/api/auth/login';
+    var response = await http.post(url, body: data);
+    if(response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print('Response : ${jsonResponse}');
+      if(jsonResponse != null) {
+        setState(() {
+          _isLoading = false;
+        });
+        sharedPreferences.setString("token", jsonResponse['result']['access_token']);
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => HomePage()), (route) => false);
+      }
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      print(response.body);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,29 +66,46 @@ class _LoginPageState extends State<LoginPage> {
                   children: <Widget>[
                     Image.asset('assets/icon/warungapp.png', height: 80),
                     SizedBox(height: 40),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25)
+                          ),
                         ),
                       ),
                     ),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25)
+                          ),
                         ),
                       ),
                     ),
-
+                    RaisedButton(
+                      child: Text('Login'),
+                      onPressed: emailController.text == "" || passwordController.text == "" ? null : () {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        signIn(emailController.text, passwordController.text);
+                      }
+                    )
                   ],
                 )
               ],
