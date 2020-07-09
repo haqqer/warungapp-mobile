35863b1cf2bf875fe23f644db41f1469103fa41c
haqqer <ilhamserbaguna@gmail.com>
2020-07-09 09:16:30 +0700

    add endpoint url

---
+++ lib/data/endpoint.dart
@@ -0,0 +1,15 @@
+/// Base Url
+const BASE_URL_DEV = 'http://192.168.1.22:8000/api'; //'http://127.0.0.1:8000'; 
+const BASE_URL_PROD = 'https://newapi.bukusaku.com';
+
+/// Auth
+const LOGIN = '/auth/login';
+const USER_DATA = '/auth/me';
+
+/// Pegawai
+const EMPLOYEE = '/pegawai';
+
+const UPLOAD = 'http://192.168.1.22:8000/upload';
+const WARUNG = '/warungs';
+const FOOD = '/foods';
+const COMMENT = '/comment';
