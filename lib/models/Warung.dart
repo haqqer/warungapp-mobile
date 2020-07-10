9e9fa88e686b98f66f89b8501994846642eb636d
haqqer <ilhamserbaguna@gmail.com>
2020-07-10 10:40:12 +0700

    update Warung model

--- lib/models/Warung.dart
+++ lib/models/Warung.dart
@@ -1,3 +1,4 @@
+import 'package:warungapp/models/Comment.dart';
 import 'package:warungapp/models/Food.dart';
 
 class Warung {
@@ -8,10 +9,12 @@ class Warung {
   String address;
   double latitude;
   double longitude;
+  double average;
   List<WarungPhoto> photos;
   List<Food> foods;
+  List<Comment> comments;
   
-  Warung({this.id, this.userId, this.name, this.description, this.address,  this.latitude, this.longitude, this.photos, this.foods});
+  Warung({this.id, this.userId, this.name, this.description, this.address,  this.latitude, this.longitude, this.photos, this.foods, this.comments, this.average});
 
   Warung.fromJson(Map<String, dynamic> json)
     : id = json['id'],
@@ -21,8 +24,10 @@ class Warung {
       address = json['address'],
       latitude = json['latitude'],
       longitude = json['longitude'],
+      average = json['average'] != null ? double.parse(json['average']) : null,
       photos = parsePhotos(json['photos']),
-      foods = parseFoods(json['foods']);
+      foods = parseFoods(json['foods']),
+      comments = parseComments(json['comments']);
       
   static List<WarungPhoto> parsePhotos(List<dynamic> items) {
     return items.map<WarungPhoto>((item) => WarungPhoto.fromJson(item)).toList();
@@ -30,6 +35,10 @@ class Warung {
 
   static List<Food> parseFoods(List<dynamic> items) {
     return items.map<Food>((item) => Food.fromJson(item)).toList();
+  }
+
+  static List<Comment> parseComments(List<dynamic> items) {
+    return items.map<Comment>((item) => Comment.fromJson(item)).toList();
   }
   Map<String, dynamic> toJson() => {
     'user_id': userId,
